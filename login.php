dbdfca7faf559dfb6c3c42f1a3c63c2429af1f14
tech-souvik <officialsouvikmaji@gmail.com>
2023-04-28 23:39:48 +0530

    Add Login Page

--- login.php
+++ login.php
@@ -1,28 +1,26 @@
 <?php
 
-$succes=0;
-$user=0;
+$login=0;
+$invalid=0;
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
     include 'connect.php';
     $username=$_POST['username'];
     $password=$_POST['password'];
 
-    $sql="SELECT `username` FROM `registration` WHERE `username`='$username' ";
+    $sql="SELECT * FROM `registration` WHERE `username`='$username' and `password`='$password'  ";
     $result=mysqli_query($con,$sql);
     if($result){
         $num=mysqli_num_rows($result);
         if($num>0){
-            $user=1;
+            $login=1;
+            session_start();
+            $_SESSION['username']=$username;
+            header('location:home.php');
+        }else{
+            $invalid=1;
         }
-        else{
-            $sql="INSERT INTO `registration`(`username`, `password`) VALUES ('$username','$password')";
-            $result=mysqli_query($con,$sql);
-            if($result){
-                $succes=1;
-            }
-        }
-    }
+      }
 }
 
 ?>
@@ -31,29 +29,21 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sign Up Page</title>
+    <title>Log In Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
   <body>
-    <h1 class="text-center">Sign Up Page</h1>
+    <h1 class="text-center">Log In Page</h1>
     <div class="container mt-5">
-        <?php
-        if($user){
+    <?php
+        if($invalid){
             echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>Ohh No Sorry!</strong> User Already Exits.
+            <strong>Ohh No Sorry!</strong> Invalid Password Or Username.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>';
         }
         ?>
-        <?php
-        if($succes){
-            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>Nice!</strong> You Are Successfully Registered.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>';
-        }
-        ?>
-        <form action="sign.php" method="post">
+        <form action="login.php" method="post">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Username</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
@@ -62,9 +52,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" name="password">
             </div>
-            <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+            <button type="submit" class="btn btn-primary w-100">Log In</button>
         </form>
-        <div align="center" class="mt-2"><a href="sign.php" style="color:green;font-style:bold;text-decoration:none;">Already Have An Account!!</a></div>
+        <div align="center" class="mt-2"><a href="sign.php" style="color:red;font-style:bold;text-decoration:none;">Don't Have Any Account?</a></div>
     </div>
   </body>
 </html>
